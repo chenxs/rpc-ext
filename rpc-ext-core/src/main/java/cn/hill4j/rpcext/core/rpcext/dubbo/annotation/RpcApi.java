d7a5382bd41a2fadce5f9ee94ecaba197ea03ec7
hillchen <chenxiaoshan@duiba.com.cn>
2019-09-10 08:12:07 +0800

    更新组件直连代码

--- rpc-ext-core/src/main/java/cn/hill4j/rpcext/core/rpcext/dubbo/annotation/RpcApi.java
+++ rpc-ext-core/src/main/java/cn/hill4j/rpcext/core/rpcext/dubbo/annotation/RpcApi.java
@@ -17,91 +17,91 @@ import java.lang.annotation.Target;
 public @interface RpcApi {
     /**
      * 服务版本
-     * @return
+     * @return 版本号
      */
     String version() default "";
 
     /**
      * 服务自定义路经
-     * @return
+     * @return 服务自定义路经
      */
     String path() default "";
 
     /**
      * 延迟注册时间
-     * @return
+     * @return 延迟注册时间
      */
     int delay() default 0;
 
     /**
      * 超时时间
-     * @return
+     * @return 超时时间
      */
     int timeout() default 0;
 
     /**
-     * 重试次数
-     * @return
+     * 超时时间
+     * @return 超时时间
      */
     int retries() default 0;
 
     /**
      * 服务端服务的最大连接数
-     * @return
+     * @return 服务端服务的最大连接数
      */
     int connections() default 0;
 
     /**
      * 负载均衡策略
-     * @return
+     * @return 负载均衡策略
      */
     String loadbalance() default "";
 
     /**
      * 异步执行
-     * @return
+     * @return 异步执行
      */
     boolean async() default false;
 
     /**
      * 是否注册到注册中心
-     * @return
+     * @return 是否注册到注册中心
      */
     boolean register() default true;
 
     /**
      * 访问日志
-     * @return
+     * @return 访问日志
      */
     String accesslog() default "";
 
     /**
      * 集群容错
-     * @return
+     * @return 集群容错
      */
     String cluster() default "";
 
     /**
      * 协议
-     * @return
+     * @return 协议
      */
     String[] protocol() default {};
 
     /**
      * 客户端使用时检测
-     * @return
+     * @return 客户端使用时检测
      */
     boolean check() default false;
 
     /**
      * 生成动态代理方式
-     * @return
+     * @return 生成动态代理方式
      */
     String proxy() default "";
 
     /**
      * 每服务消费者每服务每方法最大并发调用数
-     * @return
+     * @return 每服务消费者每服务每方法最大并发调用数
      */
     int actives() default 0;
 }
